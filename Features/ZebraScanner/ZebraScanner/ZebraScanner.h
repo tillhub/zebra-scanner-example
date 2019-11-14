2899502c02a4624952ee7c60c9ed0e5b96aec4d9
Baris <baris.atamer@tillhub.de>
2019-11-14 10:23:18 +0100

    Initial commit

---
+++ Features/ZebraScanner/ZebraScanner/ZebraScanner.h
@@ -0,0 +1,19 @@
+//
+//  ZebraScanner.h
+//  ZebraScanner
+//
+//  Created by Baris Atamer on 11/14/19.
+//  Copyright © 2019 Tillhub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZebraScanner.
+FOUNDATION_EXPORT double ZebraScannerVersionNumber;
+
+//! Project version string for ZebraScanner.
+FOUNDATION_EXPORT const unsigned char ZebraScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZebraScanner/PublicHeader.h>
+
+
---
+++ Features/ZebraScanner/ZebraScannerTests/ZebraScannerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZebraScannerTests.swift
+//  ZebraScannerTests
+//
+//  Created by Baris Atamer on 11/14/19.
+//  Copyright © 2019 Tillhub. All rights reserved.
+//
+
+import XCTest
+@testable import ZebraScanner
+
+class ZebraScannerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
